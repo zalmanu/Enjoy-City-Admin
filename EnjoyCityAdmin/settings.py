6e61ca368fefdc0529f61ff0d35406a4813e3769
Emanuel Danci <danci.emanuel@gmail.com>
2014-10-19 13:23:49 +0300

    List locations and edit location.

--- EnjoyCityAdmin/settings.py
+++ EnjoyCityAdmin/settings.py
@@ -15,7 +15,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django_mongodb_engine', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'enjoy_city_admin',                      # Or path to database file if using sqlite3.
+        'NAME': 'enjoy',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': '',
         'PASSWORD': '',
@@ -38,7 +38,7 @@ TIME_ZONE = 'America/Chicago'
 # http://www.i18nguy.com/unicode/language-identifiers.html
 LANGUAGE_CODE = 'en-us'
 
-SITE_ID = '544290579276d26f2068afbd'
+SITE_ID = '544385ff9276d27f317ddb9b'
 
 # If you set this to False, Django will make some optimizations so as not
 # to load the internationalization machinery.
